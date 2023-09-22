21b2bf0ece2569e3ddcddf56d7dd2d77fb1ae376
Moisessv <2120200388@soy.utj.edu.mx>
2023-09-21 21:57:40 -0500

    proyecto comics

---
+++ app/Models/calificaciones_comentarios.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class calificaciones_comentarios extends Model
+{
+    use HasFactory;
+}
